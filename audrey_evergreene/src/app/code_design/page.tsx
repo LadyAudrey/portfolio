ca87cd7e2c97b3068aee2c7b265e33782c65434d
LadyAudrey <theaudreygreene@gmail.com>
2023-10-05 09:22:58 -0700

    correcting apostrophes

--- audrey_evergreene/src/app/code_design/page.tsx
+++ audrey_evergreene/src/app/code_design/page.tsx
@@ -6,9 +6,9 @@ export default function page() {
           <img src="/misc/chess_king.jpg" className="graphic" />
           <h2 className="dualHeader">Coding</h2>
           <p>
-            I like coding and puzzles and the scavenger hunt. It's fun to brain
-            storm and then explore what's possible with these dynamic,
-            well-developed tools. I'm grateful to be standing on the shoulders
+            I like coding and puzzles and the scavenger hunt. It&apos;s fun to brain
+            storm and then explore what&apos;s possible with these dynamic,
+            well-developed tools. I&apos;m grateful to be standing on the shoulders
             of many coders before me. I get to enjoy the tools that coders
             wished existed, figured out how to create, and then I get to use
             those tools to develop the next generation. How cool is that???
@@ -18,7 +18,7 @@ export default function page() {
           <h2 className="dualHeader">Designing</h2>
           <p className="pr-10">
             I love creating things. Websites, vector graphics, GIFs, stickers,
-            photoshop... you name it. Playing with the tools, exploring what's
+            photoshop... you name it. Playing with the tools, exploring what&apos;s
             possible and how to get different tools to interact in interesting
             ways is what makes me the happiest.
           </p>
