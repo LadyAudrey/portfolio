281d4b2fcab2b8e858f386630c085b151b81be3f
LadyAudrey <theaudreygreene@gmail.com>
2023-10-22 15:55:28 -0700

    cleaning up after forgetting to push 2 weeks ago. It currently runs as expected

--- audrey_evergreene/src/app/layout.tsx
+++ audrey_evergreene/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
 import Nav from "./components/SocialMediaLinks";
+import Info from "./components/Info";
 import Menu from "./components/Menu";
 import CornerGraphic from "./components/CornerGraphic";
 
@@ -13,17 +14,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-w-screen min-h-screen bg-gradient-to-b from-blue-950 to-green-950 text-white">
-        <div className="fixed top-5 left-5 w-1/2 h-auto">
-          <Link href="/" replace>
-            {" "}
-            <img src="/cornerGraphic/cornerGraphic.svg" alt="graphic" />
-          </Link>
-        </div>
-        <div className="fixed flex top-5 right-10">
-          <Menu />
+        <div className="absolute top-5 left-5 w-1/2 h-auto">
+          <img src="/cornerGraphic/cornerGraphic.svg" alt="graphic" />
         </div>
         <div className="min-w-full md:pt-40 md:px-60 flex flex-row justify-between container">
           <div>{children}</div>
+          <div>
+            <Menu />
+          </div>
         </div>
         <p className="fixed bottom-3 text-white italic opacity-80">
           @2023 Created by Audrey Evergreene
