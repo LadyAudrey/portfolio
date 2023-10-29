43099447535d005a9cacbbf47f9e12fe8d8c77a2
LadyAudrey <theaudreygreene@gmail.com>
2023-10-29 11:49:36 -0700

    paths are working, hamburger drop down successful

--- audrey_evergreene/src/app/components/CornerGraphic.tsx
+++ audrey_evergreene/src/app/components/CornerGraphic.tsx
@@ -1,14 +1,7 @@
-import Image from "next/image";
-
 export default function CornerGraphic() {
   return (
-    <div>
-      <Image
-        src="/cornerGraphic/cornerGraphic.svg"
-        height={700}
-        width={700}
-        alt="lower left graphic"
-      />
+    <div className="fixed right-5 bottom-5 md:h-1/2 rotate-180 opacity-40">
+      <img src="/cornerGraphic/cornerGraphic.svg" alt="lower left graphic" />
     </div>
   );
 }
