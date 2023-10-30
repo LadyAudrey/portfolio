444e7a954b54d2d334f9cb958fb7a5eb41a4c2a4
LadyAudrey <theaudreygreene@gmail.com>
2023-10-29 22:28:53 -0700

    using corner component for both corners

--- audrey_evergreene/src/app/components/CornerGraphic.tsx
+++ audrey_evergreene/src/app/components/CornerGraphic.tsx
@@ -1,6 +1,6 @@
 export default function CornerGraphic() {
   return (
-    <div className="fixed right-5 bottom-5 md:h-1/2 rotate-180 opacity-40">
+    <div className="opacity-40">
       <img src="/cornerGraphic/cornerGraphic.svg" alt="lower left graphic" />
     </div>
   );
