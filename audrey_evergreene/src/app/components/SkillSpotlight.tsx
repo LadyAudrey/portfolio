69e2f747f9fc9052432d3832dceb35b2c6947eeb
LadyAudrey <theaudreygreene@gmail.com>
2023-11-09 19:17:26 -0800

    adding layers to the portfolio structure

---
+++ audrey_evergreene/src/app/components/SkillSpotlight.tsx
@@ -0,0 +1,14 @@
+export function SkillSpotlight() {
+  return (
+    <div className="flex flex-row justify-between">
+      <button>React</button>
+      <button>Next</button>
+      <button>Tailwind</button>
+      <button>Raw CSS</button>
+      <button>OOP</button>
+      <button>Live Sites</button>
+      <button>Completed</button>
+      <button>WIPs</button>
+    </div>
+  );
+}
