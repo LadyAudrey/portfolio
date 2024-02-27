d6f75ff42167bd77168012a7c6ff15809e553f98
LadyAudrey <theaudreygreene@gmail.com>
2024-02-26 17:21:19 -0800

    &apos; out apostrophes

--- audrey_evergreene/src/app/components/blogPosts/EngineersHavePower.tsx
+++ audrey_evergreene/src/app/components/blogPosts/EngineersHavePower.tsx
@@ -13,9 +13,9 @@ function EngineersHavePower() {
       <h3 className="text-center underline">Engineers Have Power</h3>
       <p>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As
-        a Millennial junior software developer, I&apos've been immersed in the
-        world of technology without fully grasping the global impact. It's
-        fascinating to think about how software, once viewed as an&nbsp;
+        a Millennial junior software developer, I&apos;ve been immersed in the world
+        of technology without fully grasping the global impact. It&apos;s fascinating
+        to think about how software, once viewed as an&nbsp;
         <em className="text-emerald-100">unavoidable necessity</em> has become a
         &nbsp;
         <em className="text-emerald-100">transformative tool&nbsp;</em>for so
@@ -32,12 +32,12 @@ function EngineersHavePower() {
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Today,
         brick-and-mortar businesses no longer dominate our lives. The design,
         functionality, and emotional resonance of an app can rival physical
-        interactions, shaping our daily lives in profound ways. It's incredible
+        interactions, shaping our daily lives in profound ways. It&apos;s incredible
         to witness the unprecedented global interaction with software, where a
         significant portion of the world population relies on universal products
         for their daily processes.
         <b>
-          Never before have engineers' decisions had such an immediate impact on
+          Never before have engineers&apos; decisions had such an immediate impact on
           humanity.
         </b>
       </p>
@@ -53,8 +53,8 @@ function EngineersHavePower() {
             led to cascading trends creating a lack of disconnection
           </em>
         </b>
-        . While the engineer behind it couldn't have foreseen its cultural
-        impact, it's time to acknowledge the importance of approaching
+        . While the engineer behind it couldn&apos;t have foreseen its cultural
+        impact, it&apos;s time to acknowledge the importance of approaching
         technological innovations with intention and responsibility.
       </p>
 
@@ -64,7 +64,7 @@ function EngineersHavePower() {
         products that shape our digital landscape. The speed at which solutions
         are conceived and implemented, often without direct interactions with
         users, is truly remarkable. While challenges persist and political
-        intricacies abound, what's truly remarkable is the growing number of
+        intricacies abound, what&apos;s truly remarkable is the growing number of
         individuals globally equipped with the perspective to address these
         issues. Digital solutions now emerge from living rooms and affect a
         fraction of the world population.
@@ -81,9 +81,9 @@ function EngineersHavePower() {
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As developers, we
           wield immense power in shaping our digital future. With each
           application, we have the potential of changing culture for
-          generations. Let's chart an ethical course in software development,
+          generations. Let&apos;s chart an ethical course in software development,
           prioritizing transparency, connection, and accountability. In this
-          data-driven world, let's never lose sight of our humanity. Let's build
+          data-driven world, let&apos;s never lose sight of our humanity. Let&apos;s build
           not just better software, but user experiences, designed to be kind
           human nervous systems. Guided by empathy, compassion, and a commitment
           to social good, we can create a culture of connection and mutual
@@ -93,7 +93,7 @@ function EngineersHavePower() {
       <div>
         <p className="text-3xl text-center text-amber-100">
           <em>
-            Together, let's embrace our responsibility and forge a path toward a
+            Together, let&apos;s embrace our responsibility and forge a path toward a
             brighter future.
           </em>
         </p>
