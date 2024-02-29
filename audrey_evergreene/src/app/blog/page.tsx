35806d278affe48443e25b5cfcdc84709b0da2b4
LadyAudrey <theaudreygreene@gmail.com>
2024-02-29 08:18:14 -0800

    fixing build errors

--- audrey_evergreene/src/app/blog/page.tsx
+++ audrey_evergreene/src/app/blog/page.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
 
-import { Arrows } from "../components/Arrows";
+// import { Arrows } from "../components/Arrows";
 
 // hard code importing blog posts I want to learn webpack to update this more effectively
 import { blogMetaData } from "../components/blogPosts/EngineersHavePower";
-const EHPcontent: {} = blogMetaData;
+// const EHPcontent: {} = blogMetaData;
 const { element, tags } = blogMetaData;
 
 import { blog2MetaData } from "../components/blogPosts/post2";
-const blog2content: {} = blog2MetaData;
+// const blog2content: {} = blog2MetaData;
 const { element2, tags2 } = blog2MetaData;
 
 // save above data in a format that will be similar to what I'll import with webpack
@@ -21,7 +21,7 @@ const blogContent = [
 console.log(blogContent[0].element);
 
 export default function page() {
-  const [viewItem, setViewItem] = useState(0);
+  // const [viewItem, setViewItem] = useState(0);
   // return <h1>I'm a placeholder</h1>;
   return (
     <div className="relative">
